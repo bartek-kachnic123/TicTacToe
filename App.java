65b5eb11bd46f5068fb264bc9dabb1bb7421bb71
bartlomiej.kachnic@student.uj.edu.pl <you@example.com>
2022-12-29 18:01:02 +0100

    Added ActionListener

--- App.java
+++ App.java
@@ -19,11 +19,12 @@ public class App {
 }
 
 
-class AppGUI extends JFrame {
-    static final int BOARD_SIZE = 3;
-    MyButton[][] board;
+class AppGUI extends JFrame implements ActionListener {
+    static final int BOARD_SIZE;
+    JPanel board;
+    JButton[][] buttons;
     static {
-
+        BOARD_SIZE = 3;
     }
     AppGUI() {
 
@@ -31,28 +32,45 @@ class AppGUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setSize(400, 400);
-        this.setVisible(true);
         this.getContentPane().setBackground(new Color(120, 50, 250));
+        
 
+        createBoard();
+        
+        this.setVisible(true);
+
+    }
+
+    void createBoard() {
+
+        board = new JPanel();
+        board.setLayout(new GridLayout(BOARD_SIZE, BOARD_SIZE));
+        buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
+
+        for (JButton[] row : buttons) {
+            for (JButton button : row) {
+                button = new JButton();
+                button.addActionListener(this);
+                board.add(button);
+            }
+        }
+
+        this.add(board);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        JButton button = (JButton) e.getSource();
+        button.setText("X");
+        button.setEnabled(false);
+        button.setFocusable(false);
         
     }
 
-    void initializeBoard() {
-        board = new MyButton[BOARD_SIZE][BOARD_SIZE];
-    }
+
+    
 
 
-    class MyButton extends JButton implements ActionListener {
-        
-        MyButton()
-        {
-            super();
-        }
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-            
-        }
-    }
 }
 
