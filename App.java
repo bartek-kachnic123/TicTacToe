03a1f4277e336ed5bc98c68bea28bf19b5cc52e3
bartlomiej.kachnic@student.uj.edu.pl <you@example.com>
2023-01-03 13:35:52 +0100

    Added createMenu method

--- App.java
+++ App.java
@@ -22,6 +22,10 @@ public class App {
 
 class AppGUI extends JFrame implements ActionListener {
     static final int BOARD_SIZE;
+    JPanel menu;
+    JButton newGameButton;
+    JButton endButton;
+    JButton aboutButton;
     JPanel board;
     JButton[] buttons;
     String actual_sign;
@@ -36,9 +40,10 @@ class AppGUI extends JFrame implements ActionListener {
         actual_sign = "O";
         computer = new Computer("X");
         
-
+        createMenu();
         createBoard();
         
+        this.pack();
         this.setVisible(true);
 
     }
@@ -46,14 +51,47 @@ class AppGUI extends JFrame implements ActionListener {
     {
         this.setTitle("TicTacToe");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setSize(400, 400);
+        //this.setResizable(false);
+        this.setPreferredSize(new Dimension(700, 500));
+        this.setLayout(new BorderLayout());
 
         movesCounter = 0;
+    }
+    void createMenu() {
+        menu = new JPanel();
+        menu.setPreferredSize(new Dimension(200, 500));
+
+        menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
+
+        newGameButton = new JButton();
+        newGameButton.setPreferredSize(new Dimension(200, 50));
+        newGameButton.setText("Nowa gra");
+        newGameButton.setFont(new Font("Arial", Font.PLAIN, 30));
+
+        menu.add(newGameButton);
+
+        aboutButton = new JButton();
+        aboutButton.setPreferredSize(new Dimension(200, 50));
+        aboutButton.setFont(new Font("Arial", Font.PLAIN, 30));
+        aboutButton.setText("O grze");
+        menu.add(aboutButton);
+
+        endButton = new JButton();
+        endButton.setPreferredSize(new Dimension(200, 50));
+        endButton.setFont(new Font("Arial", Font.PLAIN, 30));
+        endButton.setText("Zakoncz");
+        menu.add(endButton);
+        
+        this.add(menu, BorderLayout.WEST);
+        
+        
+        
     }
     void createBoard() {
 
         board = new JPanel();
+        board.setPreferredSize(new Dimension(500, 500));
+
         board.setLayout(new GridLayout(BOARD_SIZE, BOARD_SIZE));
         buttons = new JButton[BOARD_SIZE*BOARD_SIZE];
         
@@ -63,13 +101,14 @@ class AppGUI extends JFrame implements ActionListener {
                 board.add(buttons[i]);
 
         }
-    
-        this.add(board);
+        
+        this.add(board, BorderLayout.CENTER);
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if (gameOver()) return;
+
         JButton button = (JButton) e.getSource();
         
         button.setEnabled(false);
@@ -77,9 +116,21 @@ class AppGUI extends JFrame implements ActionListener {
 
         button.setFont(new Font("Arial", Font.BOLD, button.getHeight()));
         button.setText(actual_sign);
+        ++movesCounter;
         
-        computer.makeRandomMove(buttons);
+        computer.makeRandomMove(buttons, movesCounter);
+        ++movesCounter;
         
+    }
+
+    public boolean gameOver() {
+
+        if (movesCounter > buttons.length) return true;
+
+        return false;
+    }
+    public boolean checkWin() {
+        return false;
     }
 
 
@@ -96,12 +147,13 @@ class Computer {
 
     }
 
-    void makeRandomMove(JButton[] buttons) {
+    void makeRandomMove(JButton[] buttons, int movesCounter) {
         Random random = new Random();
         int randomNumber;
-        System.out.println(buttons[1]);
+
         do {
             randomNumber = random.nextInt(buttons.length);
+            if (movesCounter >= buttons.length) return;
         }while(!buttons[randomNumber].isEnabled());
 
         buttons[randomNumber].setFont(new Font("Arial", Font.BOLD, buttons[randomNumber].getHeight()));
